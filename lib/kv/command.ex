67863c0d58e934eff13193ee8156e68eb67b335b
Caio Reidt <caio@caioreidt.dev>
2024-11-20 20:22:41 -0300

    validação do comando SET

--- lib/kv/command.ex
+++ lib/kv/command.ex
@@ -21,18 +21,46 @@ defmodule Kv.Command do
     if is_list(params), do: [command, ""], else: [command, params]
   end
 
+  # Validar e executar o comando GET
   defp execute(:GET, param_tokens) do
     with :ok <- validate_command(:GET, param_tokens) do
       {:ok, "-"}
     end
   end
 
+  # Validar e executar o comando SET
+  defp execute(:SET, param_tokens) do
+    with :ok <- validate_command(:SET, param_tokens) do
+      {:ok, "NIL -"}
+    end
+  end
+
+  # Validar o comando GET
+  # O comando deve sempre receber apenas um parâmetro (<chave>)
+  # O parâmetro <chave> deve ser uma string
   @spec validate_command(:GET, list()) :: :ok | {:err, binary()}
   def validate_command(:GET, param_tokens) do
     if length(param_tokens) == 1 and is_binary(Enum.at(param_tokens, 0)) do
       :ok
     else
       {:err, "ERR GET <chave> - Syntax Error"}
+    end
+  end
+
+  # Validar o comando SET
+  # O comando deve sempre receber apenas 2 parâmetros (<chave> e <valor>)
+  # O parâmetro <chave> deve ser uma string
+  # O parâmetro <valor> pode ser uma string, um booleano ou um inteiro (todos menos :NIL)
+  @spec validate_command(:SET, list()) :: :ok | {:err, binary()}
+  def validate_command(:SET, param_tokens) do
+    if (
+      length(param_tokens) == 2 # Apenas 2 parâmetros
+      and is_binary(Enum.at(param_tokens, 0)) # <chave> deve ser uma string
+      and Enum.at(param_tokens, 1) != :NIL # <valor> não pode ser :NIL
+    ) do
+      :ok
+    else
+      {:err, "ERR SET <chave> <valor> - Syntax Error"}
     end
   end
 end
