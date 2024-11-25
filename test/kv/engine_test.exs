409315bd3b59f97b1a8e2f7dabded5f3c76a4ee1
Caio Reidt <caio@caioreidt.dev>
2024-11-24 23:49:53 -0300

    Testes

--- test/kv/engine_test.exs
+++ test/kv/engine_test.exs
@@ -2,190 +2,179 @@ defmodule KV.EngineTest do
   use ExUnit.Case, async: true
 
   alias KV.Engine
-
-  # Criar arquivos com nomes aleatórios para isolar testes
-  defp random_test_file do
-    path =
-      "tmp/test"
-      |> String.split("/")
-      |> Enum.reduce("",
-        fn dir, agg_path ->
-          current_dir = agg_path <> "#{dir}/"
-
-          if !File.exists?(current_dir) do
-            File.mkdir(current_dir)
-          end
-
-          current_dir
-        end)
-
-    rand = for _ <- 1..10, into: "", do: <<Enum.random(~c"0123456789abcdefghijklmnopqrstuvwxyz")>>
-    path <> "#{rand}.db"
-  end
+  alias KV.DatabaseEngineHelper
 
   describe "get/2" do
     test "reads strings from database file" do
-      Engine.start_link(random_test_file())
-      Engine.put("client", "teste", "teste123")
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      assert Engine.get("client", "teste") == "teste123"
+      Engine.put(client, key, "teste123")
+
+      assert Engine.get(client, key) == "teste123"
     end
 
     test "reads integers from database file" do
-      Engine.start_link(random_test_file())
-      Engine.put("client", "_int", 123)
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      assert Engine.get("client", "_int") == 123
+      Engine.put(client, key, 123)
+
+      assert Engine.get(client, key) == 123
     end
 
     test "reads booleans from database file" do
-      Engine.start_link(random_test_file())
-      Engine.put("client", "_true", true)
-      Engine.put("client", "_false", false)
+      client = DatabaseEngineHelper.get_random_string()
+      key_1 = DatabaseEngineHelper.get_random_string()
+      key_2 = DatabaseEngineHelper.get_random_string()
 
-      assert Engine.get("client", "_true") == true
-      assert Engine.get("client", "_false") == false
+      Engine.put(client, key_1, true)
+      Engine.put(client, key_2, false)
+
+      assert Engine.get(client, key_1) == true
+      assert Engine.get(client, key_2) == false
     end
   end
 
   describe "put/3" do
     test "puts strings into the database file" do
-      Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      assert Engine.put("client", "teste", "teste123") == {nil, "teste123"}
-      assert Engine.put("client", "teste", "teste456") == {"teste123", "teste456"}
+
+      assert Engine.put(client, key, "teste123") == {nil, "teste123"}
+      assert Engine.put(client, key, "teste456") == {"teste123", "teste456"}
     end
 
     test "puts integers into the database file" do
-      Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      assert Engine.put("client", "_int", 123) == {nil, 123}
-      assert Engine.put("client", "_int", 456) == {123, 456}
+
+      assert Engine.put(client, key, 123) == {nil, 123}
+      assert Engine.put(client, key, 456) == {123, 456}
     end
 
     test "puts booleans into the database file" do
-      Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      assert Engine.put("client", "_bool", true) == {nil, true}
-      assert Engine.put("client", "_bool", 456) == {true, 456}
+
+      assert Engine.put(client, key, true) == {nil, true}
+      assert Engine.put(client, key, 456) == {true, 456}
     end
   end
 
   describe "begin_transaction/1" do
     test "stars a new map for the client" do
-      {:ok, pid} = Engine.start_link(random_test_file())
-      Engine.begin_transaction("client")
+      client = DatabaseEngineHelper.get_random_string()
+      Engine.begin_transaction(client)
 
-      %{transactions: transactions} = :sys.get_state(pid)
-      assert Map.has_key?(transactions, "client")
-    end
-
-    test "gets the current file offset" do
-      {:ok, pid} = Engine.start_link(random_test_file())
-
-      # offset = 8 bytes + key length = uint16 + uint32 + key + uint8
-      Engine.put("client", "_", true)
-      Engine.put("client", "_", true)
-
-      Engine.begin_transaction("client")
-
-      %{transactions: %{"client" => %{offset: offset}}} = :sys.get_state(pid)
-      assert offset == 18
+      %{transactions: transactions} = :sys.get_state(GenServer.whereis(Engine))
+      assert Map.has_key?(transactions, client)
     end
 
     test "transaction puts data in-memory only" do
-      {:ok, pid} = Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
+      key_1 = DatabaseEngineHelper.get_random_string()
+      key_2 = DatabaseEngineHelper.get_random_string()
 
       # offset = 8 bytes + key length = uint16 + uint32 + key + uint8
-      Engine.put("client", "key_1", 123)
-      Engine.put("client", "key_2", 456)
+      Engine.put(client, key_1, 123)
+      Engine.put(client, key_2, 456)
 
-      Engine.begin_transaction("client")
+      Engine.begin_transaction(client)
 
-      {old_value, new_value} = Engine.put("client", "key_2", 789)
+      {old_value, new_value} = Engine.put(client, key_2, 789)
 
       assert old_value == 456
       assert new_value == 789
 
-      %{transactions: %{"client" => %{
-        values: %{"key_2" => transaction_value}}
-      }} = :sys.get_state(pid)
+      %{transactions: %{^client => %{
+        values: %{^key_2 => transaction_value}}
+      }} = :sys.get_state(GenServer.whereis(Engine))
 
       assert transaction_value == 789
     end
 
     test "other clients can't see values in the transaction" do
-      Engine.start_link(random_test_file())
+      client_1 = DatabaseEngineHelper.get_random_string()
+      client_2 = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
+
 
       # offset = 8 bytes + key length = uint16 + uint32 + key + uint8
-      Engine.put("client-A", "key", 123)
-      Engine.begin_transaction("client-A")
-      Engine.put("client-A", "key", 456)
+      Engine.put(client_1, key, 123)
+      Engine.begin_transaction(client_1)
+      Engine.put(client_1, key, 456)
 
-      assert Engine.get("client-A", "key") == 456
-      assert Engine.get("client-B", "key") == 123
+      assert Engine.get(client_1, key) == 456
+      assert Engine.get(client_2, key) == 123
     end
 
     test "fails if a transaction already exists" do
-      Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
 
-      Engine.begin_transaction("client")
-      assert Engine.begin_transaction("client") == {:error, :active_transaction}
+      Engine.begin_transaction(client)
+      assert Engine.begin_transaction(client) == {:error, :active_transaction}
     end
   end
 
   describe "rollback_transaction/1" do
     test "deletes the map for the client" do
-      {:ok, pid} = Engine.start_link(random_test_file())
-      Engine.begin_transaction("client")
-      Engine.rollback_transaction("client")
+      client = DatabaseEngineHelper.get_random_string()
 
-      %{transactions: transactions} = :sys.get_state(pid)
-      assert Map.has_key?(transactions, "client") == false
+      Engine.begin_transaction(client)
+      Engine.rollback_transaction(client)
+
+      %{transactions: transactions} = :sys.get_state(GenServer.whereis(Engine))
+      assert Map.has_key?(transactions, client) == false
     end
 
     test "it does't affect information outside the transaction" do
-      Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      Engine.put("client", "_", true)
-      Engine.begin_transaction("client")
-      Engine.put("client", "_", false)
+      Engine.put(client, key, true)
+      Engine.begin_transaction(client)
+      Engine.put(client, key, false)
 
-      Engine.rollback_transaction("client")
-      assert Engine.put("client", "_", 123) == {true, 123}
+      Engine.rollback_transaction(client)
+      assert Engine.put(client, key, 123) == {true, 123}
     end
 
     test "fails if a transaction doesn't exist" do
-      Engine.start_link(random_test_file())
-
-      assert Engine.rollback_transaction("client") == {:error, :no_active_transaction}
+      client = DatabaseEngineHelper.get_random_string()
+      assert Engine.rollback_transaction(client) == {:error, :no_active_transaction}
     end
   end
 
   describe "commit_transaction/1" do
     test "commits changes transaction to disk" do
-      Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      Engine.put("client", "key", 12)
-      Engine.begin_transaction("client")
-      Engine.put("client", "key", 345)
-      Engine.put("client", "key", 678)
+      Engine.put(client, key, 12)
+      Engine.begin_transaction(client)
+      Engine.put(client, key, 345)
+      Engine.put(client, key, 678)
 
-      assert Engine.get("client-B", "key") == 12
+      assert Engine.get("client-B", key) == 12
 
-      Engine.commit_transaction("client")
-      assert Engine.get("client-B", "key") == 678
+      Engine.commit_transaction(client)
+      assert Engine.get("client-B", key) == 678
     end
 
     test "fails if a there is a conflict with a key newer than the transaction" do
-      Engine.start_link(random_test_file())
+      client = DatabaseEngineHelper.get_random_string()
+      key = DatabaseEngineHelper.get_random_string()
 
-      Engine.put("client", "key", 12)
-      Engine.begin_transaction("client")
-      Engine.put("client", "key", 345)
-      Engine.put("client-B", "key", 678)
+      Engine.put(client, key, 12)
+      Engine.begin_transaction(client)
+      Engine.put(client, key, 345)
+      Engine.put("client-B", key, 678)
 
-      assert Engine.commit_transaction("client") == :error
+      assert Engine.commit_transaction(client) == :error
     end
   end
 end
